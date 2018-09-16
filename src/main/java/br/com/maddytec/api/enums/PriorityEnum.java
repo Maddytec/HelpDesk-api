e5e4fd1d9fec8d7f1890608d2f307e1cd835cb81
Madson Silva <maddytec@gmail.com>
2018-09-16 14:53:54 -0300

    Commit Initial

---
+++ src/main/java/br/com/maddytec/api/enums/PriorityEnum.java
@@ -0,0 +1,8 @@
+package br.com.maddytec.api.enums;
+
+public enum PriorityEnum {
+	High,
+	Normal,
+	Low
+	
+}
---
+++ src/main/java/br/com/maddytec/api/enums/ProfileEnum.java
@@ -0,0 +1,7 @@
+package br.com.maddytec.api.enums;
+
+public enum ProfileEnum {
+	ROLE_ADMIN,
+	ROLE_CUSTUMER,
+	ROLE_TECHNICIAN
+}
